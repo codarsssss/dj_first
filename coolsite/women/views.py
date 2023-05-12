c7dc15ce8ab0c67e553c689dc915641e45d9eac2
codarsssss <codarsssss@gmail.com>
2023-05-12 21:37:27 +0300

    big deal

---
+++ coolsite/women/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
+from .models import *
+
+
+menu = ['О сайте', 'Добавить статью', 'Обратная саязь', 'Войти']
+
+
+def index(request):
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+
+
+def categories(request, catid):
+    if request.GET:
+        print(request.GET)
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{catid}</p>')
+
+
+def archive(request, year):
+    if int(year) > 2020:
+        return redirect('home', permanent=False)
+
+    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</1h>')
+
