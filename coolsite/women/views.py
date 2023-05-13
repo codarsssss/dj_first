5888d02b5f4de3720ce95d78dfd0e3288f8049bf
codarsssss <codarsssss@gmail.com>
2023-05-13 17:46:02 +0300

    slugs

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 
 
-
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+            {'title': 'Добавить статью', 'url_name': 'add_page'},
+            {'title': 'Обратная саязь', 'url_name': 'contact'},
+            {'title': 'Войти', 'url_name': 'login'}]
 
 
 def index(request):
@@ -48,18 +51,28 @@ def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</1h>')
 
 
-def show_post(request, post_id):
-    return HttpResponse(f'Отображение статье с id = {post_id}')
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+    cat_slug = Category.objects.get(pk=post.cat_id).slug
+    context = {
+        'post': post,
+        'title': post.title,
+        'cat_selected': cat_slug
+    }
+
+    return render(request, 'women/post.html', context=context)
 
 
-def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id)
+def show_category(request, cat_slug):
+    cat_id = Category.objects.get(slug=cat_slug)
+    posts = Women.objects.filter(cat_id=cat_id.pk)
 
     if len(posts) == 0:
         raise Http404()
 
     context = {'posts': posts,
                'title': 'Главная страница',
-               'cat_selected': cat_id}
+               'cat_selected': cat_slug
+    }
 
     return render(request, 'women/index.html', context=context)
