7218eec70b8fe436deb71c6a552775542972fccc
codarsssss <codarsssss@gmail.com>
2023-05-13 15:28:48 +0300

    user tags

---
+++ coolsite/women/templatetags/women_tags.py
@@ -0,0 +1,31 @@
+from django import template
+from women.models import *
+
+register = template.Library()
+
+
+@register.simple_tag(name='getcats')
+def get_categories(filter=None):
+    if not filter:
+        return Category.objects.all()
+    return Category.objects.filter(pk=filter)
+
+
+@register.inclusion_tag('women/list_categories.html')
+def show_categories(sort, cat_selected):
+    if not sort:
+        cats = Category.objects.all()
+    else:
+        cats = Category.objects.order_by(sort)
+
+    return {'cats': cats, 'cat_selected': cat_selected}
+
+
+@register.inclusion_tag('women/main_menu.html')
+def show_main_menu():
+    menu = [{'title': 'О сайте', 'url_name': 'about'},
+            {'title': 'Добавить статью', 'url_name': 'add_page'},
+            {'title': 'Обратная саязь', 'url_name': 'contact'},
+            {'title': 'Войти', 'url_name': 'login'}]
+
+    return {'menu': menu}
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -3,18 +3,12 @@ from django.shortcuts import render, redirect
 from .models import *
 
 
-menu = [{'title': 'О сайте', 'url_name': 'about'},
-        {'title': 'Добавить статью', 'url_name': 'add_page'},
-        {'title': 'Обратная саязь', 'url_name': 'contact'},
-        {'title': 'Войти', 'url_name': 'login'}]
+
 
 
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
     context = {'posts': posts,
-               'cats': cats,
-               'menu': menu,
                'title': 'Главная страница',
                'cat_selected': 0}
 
@@ -22,7 +16,7 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
 def addpage(request):
@@ -60,14 +54,11 @@ def show_post(request, post_id):
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
 
     if len(posts) == 0:
         raise Http404()
 
     context = {'posts': posts,
-               'cats': cats,
-               'menu': menu,
                'title': 'Главная страница',
                'cat_selected': cat_id}
 
